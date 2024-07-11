ab4fdd651b17119d47123d1d519ed1eaadab722a
Rauã <rauavasconcelos22@gmail.com>
2024-07-11 19:27:29 -0300

    ⚡️ Criando lista de tarefas e renderizando em um map para aparecer na pagina do projeto, por enquanto sem estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
+import { useState } from "react"
+
 function App() {
-
-  return (
-    <h1>Projeto</h1>
-  )
+ const [todos, setTodos] = useState([
+{
+  id:1,
+  text: "criar funcionalidade x no sistema",
+  category: "Trabalho",
+  isCompleted: false,
+},
+{
+  id:2,
+  text: "Ir pra academia",
+  category: "Pessoal",
+  isCompleted: false,
+},
+{
+  id:3,
+  text: "Estudar React",
+  category: "Estudos",
+  isCompleted: false,
 }
+ ]);
 
-export default App
+  return(
+    <div className="app">
+      <h1>Todo List</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p> 
+               <p className="category">
+                ({todo.category})
+               </p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>Excluir</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )}
+
+export default App;
